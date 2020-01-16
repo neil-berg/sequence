4f7e90f136e0d00f2f66bd0713652fb751bbf3d3
Neil Berg <neil.berg14@gmail.com>
2020-01-15 23:47:56 -0800

    setup prod DB on heroku

--- knexfile.js
+++ knexfile.js
@@ -36,5 +36,25 @@ module.exports = {
         seeds: {
             directory: path.join(BASE_PATH, 'seeds')
         }
+    },
+    production: {
+        client: 'pg',
+        connection: {
+            host: process.env.PG_HOST,
+            port: process.env.PG_PORT,
+            user: process.env.PG_USER,
+            password: process.env.PG_PASSWORD,
+            database: process.env.PG_DATABASE
+        },
+        pool: {
+            min: 2,
+            max: 10
+        },
+        migrations: {
+            directory: path.join(BASE_PATH, 'migrations')
+        },
+        seeds: {
+            directory: path.join(BASE_PATH, 'seeds')
+        }
     }
 };

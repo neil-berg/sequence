c531aaad703766ed5c7255acba9bd3f31572cdd4
Neil Berg <neil.berg14@gmail.com>
2020-02-17 23:16:09 -0800

    button conversion

--- src/client/components/button/Button.tsx
+++ src/client/components/button/Button.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
 
-export const Button = ({ flavor, text, ...props }) => (
-    <StyledButton {...props} flavor={flavor}>
+interface ButtonProps {
+    flavor: string;
+    background: string;
+    color: string;
+    text?: string;
+    onClick?: () => void;
+}
+
+export const Button = ({ flavor, text, ...rest }: ButtonProps) => (
+    <StyledButton {...rest} flavor={flavor}>
         {text}
     </StyledButton>
 );
 
-const StyledButton = styled.button`
+const StyledButton = styled.button<ButtonProps>`
     cursor: pointer;
     font-weight: bold;
     ${props =>
         props.flavor === 'outline' &&
-        css`
+        css<ButtonProps>`
             padding: 0.5rem 1.5rem;
             border-radius: 5px;
             transition: all 0.2s ease;
@@ -26,7 +34,7 @@ const StyledButton = styled.button`
         `}
     ${props =>
         props.flavor === 'text' &&
-        css`
+        css<ButtonProps>`
             border: 0;
             background: transparent;
             color: ${props => props.color};
